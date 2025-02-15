c090d11b1b532de45a6e6b7e21a4556443938d72
JuanJavierr <xavierpeladeau12@hotmail.com>
2025-02-15 16:00:11 -0500

    add constraint and node order to graph

--- branch-and-bound-scipy.py
+++ branch-and-bound-scipy.py
@@ -10,7 +10,7 @@ best_solution = ((None, None), float('-inf'))  # (solution, objective value)
 node_values = {}
 
 
-def branch_and_bound(x1_range=(0, None), x2_range=(0, None)):
+def branch_and_bound(x1_range=(0, None), x2_range=(0, None), parent_constraint=None):
     global node_counter, best_solution, edges, node_values
 
     c = [-1, -2]
@@ -35,7 +35,7 @@ def branch_and_bound(x1_range=(0, None), x2_range=(0, None)):
         objective = -res.fun  # Since we are minimizing the negative of the objective function
 
         # Update node_values with the current node's x, y values, and objective value
-        node_values[node_id] = (round(x1_val, 2), round(x2_val, 2), round(objective, 2))
+        node_values[node_id] = (round(x1_val, 2), round(x2_val, 2), round(objective, 2), node_id, parent_constraint)
 
         # Update the best solution if a new better integer solution is found
         if x1_val.is_integer() and x2_val.is_integer() and objective > best_solution[1]:
@@ -44,44 +44,48 @@ def branch_and_bound(x1_range=(0, None), x2_range=(0, None)):
         # Branch on x1 if not integer
         if not x1_val.is_integer():
             x1_floor = int(np.floor(x1_val))
-            edges.append((node_id, node_counter))
-            branch_and_bound(x1_range=(0, x1_floor), x2_range=x2_range)
-            edges.append((node_id, node_counter))
-            branch_and_bound(x1_range=(x1_floor+1, None), x2_range=x2_range)
+            edges.append((node_id, node_counter, f"x <= {x1_floor}"))
+            branch_and_bound(x1_range=(0, x1_floor), x2_range=x2_range, parent_constraint=f"x <= {x1_floor}")
+            edges.append((node_id, node_counter, f"x >= {x1_floor + 1}"))
+            branch_and_bound(x1_range=(x1_floor+1, None), x2_range=x2_range, parent_constraint=f"x >= {x1_floor + 1}")
 
         # Branch on x2 if not integer
         if not x2_val.is_integer():
             x2_floor = int(np.floor(x2_val))
-            edges.append((node_id, node_counter))
-            branch_and_bound(x1_range=x1_range, x2_range=(0, x2_floor))
-            edges.append((node_id, node_counter))
-            branch_and_bound(x1_range=x1_range, x2_range=(x2_floor+1, None))
+            edges.append((node_id, node_counter, f"y <= {x2_floor}"))
+            branch_and_bound(x1_range=x1_range, x2_range=(0, x2_floor), parent_constraint=f"y <= {x2_floor}")
+            edges.append((node_id, node_counter, f"y >= {x2_floor + 1}"))
+            branch_and_bound(x1_range=x1_range, x2_range=(x2_floor+1, None), parent_constraint=f"y >= {x2_floor + 1}")
     else:
         # Update node_values with the current node's x, y values, and objective value
-        node_values[node_id] = (None, None, None)
+        node_values[node_id] = (None, None, None, node_id, parent_constraint)
 
 
 def visualize_tree(edges, node_values):
     G = nx.DiGraph()
-    G.add_edges_from(edges)
+    G.add_edges_from([(u, v) for u, v, _ in edges])
     
     pos = nx.drawing.nx_agraph.graphviz_layout(G, prog='dot')
     
-    # Zeichnen Sie die Knoten (unsichtbar)
+    # Draw nodes (invisible)
     nx.draw_networkx_nodes(G, pos, node_color='white', node_size=1)
     
-    # Zeichnen Sie die Kanten
+    # Draw edges
     nx.draw_networkx_edges(G, pos)
     
-    # Zeichnen Sie die Labels mit einer Bounding-Box um jedes
+    # Draw labels with a bounding box around each
     labels = {}
     for node, values in node_values.items():
         if values[0] is None:
-            labels[node] = "Infeasible"
+            labels[node] = f"id={values[3]}, Infeasible"
         else:
-            labels[node] = f"x={values[0]}, y={values[1]}, z={values[2]}"
+            labels[node] = f"id={values[3]}, x={values[0]}, y={values[1]}, z={values[2]}"
         
     nx.draw_networkx_labels(G, pos, labels, bbox=dict(boxstyle='round,pad=0.3', edgecolor='black', facecolor='white'))
+    
+    # Draw edge labels for constraints
+    edge_labels = {(u, v): constraint for u, v, constraint in edges}
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
     
     plt.show()
 
